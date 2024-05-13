3e569860c6725d992a83865786f678622943f227
wafiq <wafiqgithub775@gmail.com>
2024-05-14 00:01:28 +0600

    FEAT: update layout.tsx

--- frontend/src/layout/Layout.tsx
+++ frontend/src/layout/Layout.tsx
@@ -1,14 +1,16 @@
 import Header from '../components/Header'
 import { Outlet } from 'react-router-dom'
+import Hero from '../components/Hero'
+import Footer from '../components/Footer'
 
 const Layout = () => {
     return (
         <div className='flex flex-col min-h-screen'>
             <Header />
-            {/* Hero */}
+            <Hero />
             {/* search bar */}
             <div className='container mx-auto py-10 flex-1'><Outlet /></div>
-            {/* footer */}
+            <Footer />
         </div>
     )
 }

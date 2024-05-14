872e8b7f8c257bff015be07390c13ea50b13d0a0
wafiq <wafiqgithub775@gmail.com>
2024-05-14 12:20:15 +0600

    FEAT: add response utility

---
+++ backend/src/utils/response.ts
@@ -0,0 +1,17 @@
+import { StatusCodes } from "http-status-codes";
+
+export const successResponse = {
+    success: true,
+    message: "Request fulfilled successfully",
+    data: {},
+    statusCode: StatusCodes.OK,
+    origin: ""
+}
+
+export const errorResponse = {
+    success: false,
+    message: "Something went wrong",
+    error: {},
+    statusCode: StatusCodes.INTERNAL_SERVER_ERROR,
+    origin: ""
+}
